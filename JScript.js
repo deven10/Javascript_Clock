a6b3f1e020fa3f48a7f0ad879a7b2451f9d8c2ef
Deven Umrania <32352481+deven10@users.noreply.github.com>
2020-10-08 17:31:39 +0530

    Add files via upload

---
+++ JScript.js
@@ -0,0 +1,23 @@
+﻿
+const second = document.getElementById('second');
+const minute = document.getElementById('minute');
+const hour = document.getElementById('hour');
+
+set Interval(updateClock,1000);
+function updateClock(){
+
+    let day =new Date();
+    let ss = day.getSeconds()*deg;
+    let mm = day.getMinutes()*deg;
+    let hh = day.getHours()*30;
+
+    hour.style.transform = 'rotateZ(${hh+(mn/12)})';
+    minute.style.transform = 'rotateZ(${mm}deg)';
+    second.style.transform = 'rotateZ(${ss}deg)';
+}
+
+updateClock();
+
+
+
+ 
